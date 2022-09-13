9ad2cd7c600cc7baa1f8e6905d4945b05196fb47
olgak169 <okrupnova@gmail.com>
2022-09-13 16:00:32 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { RouterView } from 'vue-router'
+  import HeaderComp from './components/HeaderComp.vue'
+</script>
+
+<template>
+  <HeaderComp />
+  <RouterView v-slot="{ Component }">
+    <Transition name="page">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
+</template>
