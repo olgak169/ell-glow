6b2c5b15b6d9f02606985a23eae76df36e0c5c96
olgak169 <olgak169@users.noreply.github.com>
2022-09-13 11:50:13 +0000

    deploy: 4a26e2ff3d538a6f8b7b6e0ec3183b89ddf58d90

--- assets/ContactView.8c540a59.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.ba05fa48.js";const c={};function e(n,r){return"Contact"}const _=t(c,[["render",e]]);export{_ as default};
--- assets/ServicesView.c2374223.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.ba05fa48.js";const r={};function c(s,t){return"Services"}const n=e(r,[["render",c]]);export{n as default};
