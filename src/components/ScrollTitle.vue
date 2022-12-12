fef270e237a64f33c8dded4ed5edda0df47020ae
olgak169 <okrupnova@gmail.com>
2022-12-12 15:49:55 +0600

    adjustments

--- src/components/ScrollTitle.vue
+++ src/components/ScrollTitle.vue
@@ -55,6 +55,7 @@
   })
   onBeforeUnmount(() => {
     tl.revert()
+    ScrollTrigger.killAll()
     window.removeEventListener('resize', resize)
   })
   //
@@ -62,16 +63,14 @@
 
 <style lang="scss">
   .scroll-title {
-    margin: 2rem 0;
+    margin: var(--space-m) 0;
     width: 100%;
     overflow: hidden;
     background-color: v-bind('colors[props.color].bg');
-    // background-color: #c6255a;
     span {
-      font-size: var(--fs-lg);
+      font-size: var(--step-6);
       font-weight: 300;
       color: v-bind('colors[props.color].font');
-      // color: #ff5392;
       &:nth-of-type(even) {
         font-weight: bold;
       }
